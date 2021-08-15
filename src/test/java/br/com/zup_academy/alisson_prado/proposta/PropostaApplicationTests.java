a46faa92eec2e669a8e0a4f64fefbf795ec725ca
Alisson Prado <alisson.prado@zup.com.br>
2021-08-15 18:56:36 -0300

    setup-projeto

---
+++ src/test/java/br/com/zup_academy/alisson_prado/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup_academy.alisson_prado.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
